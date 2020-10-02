14299dc0701194c59afb8ec42bb3422351b8c8d3
Andrew Almeida <andrew.l.almeida@outlook.com>
2020-10-01 21:50:36 -0300

    Padrao Adapter

---
+++ Padroes-de-Projeto/Adapter/Exercicios/Main1.java
@@ -0,0 +1,6 @@
+class Main1 {
+    public static void main(String args[]) {
+          MedidorTemperatura medidor = new AdaptadorFarenheit();
+          System.out.println("Temperatura Celsius = " + medidor.temperaturaCelsius());
+      }
+  }
